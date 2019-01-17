792700b131906b1808a1c77191ba712a7c61679b
dinh truong <dinhtruong018@gmail.com>
2019-01-17 14:04:00 +0700

    commit

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
-construction
+# Module Construction ()
+
+ | 
+------------ | -------------
+Name | Construction
+Author | ![Peter Dinh](https://github.com/peter-dinh)
+Odoo version | 12.0
+Module version | 1.0.0
+Depends | `base`, `stock`
+
+## Description:
+Hệ thống cần một module quản lý xây dựng; tập trung vào quản lý vật liệu trong xây dựng. Trong đó, hỗ trợ tồn kho trong hệ thống.
+Hệ thống đã tích hợp sẵn module Stock. Moduel quản lý xây dựng cần kế thừa module này
+
+## Relationship
+### BFD Diagram:
+
+### Model Class:
+
+### Method decorators:
+
+## Views
+
+### Menu:
+your_module
+|-- ...
+`-- tests
+    |-- __init__.py
+    |-- test_bar.py
+    `-- test_foo.py
+
+### Actions:
+
+## Testing
+Name         | Description 
+------------ | -------------
+Name | Construction
+
+## Security
+> All users have rights
+
+
+## Release: 
--- models/models.py
+++ models/models.py
@@ -4,31 +4,33 @@ from odoo import models, fields, api
 
 class Project(models.Model):
     """
-
+    Danh sách dự án của công ty
     """
     _name = 'construction.project'
 
     name = fields.Char(string='Name Project')
     commencement_date = fields.Date(string='Commencement Date')
     investor = fields.Char(string='Investor')
-    company_id = fields.Many2one('res.')
+    company_id = fields.Many2one('res.company', string="Company", default=lamda self: self.env.user.company_id.id)
     type_project = fields.Selection(selection=[('bridge', 'Bridge'), ('street', 'Street'), ('building', 'Building')])
     list_state = fields.One2many(comodel_name='construction.state_project', inverse_name='project_id', string='List State')
 
-class Result(models.Model):
+class Perform(models.Model):
     """
-    
+    Kết quả và yêu cầu của các giai đoạn thực hiện các hạng mục
     """
-    _name = 'construction.result'
+    _name = 'construction.perform'
 
     state_id = fields.Many2one('construction.state_project', string='State')
-    block_id = fields.Many2one('construction.block', string='Block')
+    block_id = fields.Many2one('construction.block', string='Block', domain=)
+    project_id = fields.Many2one('construction.project', string='Project')
+    requirement = fields.Many2one('construction.material_requirements', string='Requirements')
     result = fields.Selection(selection=[('processing', 'Processing'), ('cancel', 'Cancel'), ('success', 'Success')], required=True, default='processing')
 
 
 class State_Project(models.Model):
     """
-
+    Các giai đoạn của dự án
     """
     _name = 'construction.state_project'
 
@@ -36,36 +38,35 @@ class State_Project(models.Model):
     date_start = fields.Date(string='Date Start')
     date_end = fields.Date(string='Date End')
     project_id = fields.Many2one('construction.project', string="Project")
-    list_result = fields.One2many('construction.result', inverse_name='state_id', string='List Result')
+    list_perform = fields.One2many('construction.perform', inverse_name='state_id', string='List Result')
+    finish = fields.Boolean(string='Finish')
 
 
 class Block(models.Model):
     """
-
+    Các hạng mục của dự án 
     """
     _name = 'construction.block'
     _inherit = 'construction.material_requirements'
 
     name = fields.Char(string='Name')
     project_id = fields.Many2one('construction.project', string='Project')
-    state = fields.Many2one('construction.state_project', string='State')
-    list_requirement = fields.One2many('construction.material_requirements', inverse_name='block_id', string='Requirements')
-    list_result = fields.One2mnay('construction.result', inverse_id='block_id', string='List Result')
+    list_perform = fields.One2mnay('construction.perform', inverse_id='block_id', string='List Result')
 
 class Material_Requirements(models.Model):
     """
-
+    Danh sách yêu cầu vật tư cho giai đoạn triển khai hạng mục
     """
     _name = 'construction.material_requirements'
 
     name = fields.Char(string="Name")
-    block_id = field.Many2one('construction.block', string='Block')
+    block_id = field.Many2one('construction.block', string='Block', store=False, compute='_get_block')
     list_material = fields.One2many('construction.material_detail', inverse_name='required_id', string='List Materials')
 
     
 class Material_Detail(models.Model):
     """
-
+    Chi tiết Vật tư cần sử dụng
     """
     _name = 'construction.material_detail'
     _inherit = 'product.product'
@@ -78,8 +79,8 @@ class Material_Detail(models.Model):
 
 class Product_For_Construction(models.Model):
     """
-    
+    Thêm loại sản phẩm dành cho xây dựng.
     """
     _inherit = 'product.template'
 
-    type = fields.Selection(selection_add=[('construction', 'Construction')])
+    type = fields.Selection(selection_add=[('construction', 'Construction')], default='construction')
